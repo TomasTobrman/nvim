0e8a4a2100f7b5dc7f050f188e87a7ccf063f9d0
Tomáš Tobrman <30987354+TomasTobrman@users.noreply.github.com>
2023-11-16 14:28:41 +0100

    edited nvim-notify.lua

--- lua/tirea/plugins/nvim-notify.lua
+++ lua/tirea/plugins/nvim-notify.lua
@@ -1,5 +1,17 @@
 return {
 	'rcarriga/nvim-notify',
 	event = 'VimEnter',
-	config = true,
+	config = function()
+		local notify = require('notify')
+
+		notify.setup({
+			level = vim.log.levels.INFO,
+			render = 'wrapped-compact',
+			stages = 'slide',
+			timeout = 5000,
+			top_down = false,
+		})
+
+		vim.notify = notify
+	end,
 }
