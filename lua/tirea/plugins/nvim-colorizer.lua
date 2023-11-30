72de1cf0af716011f8bd45fc6b12854f79573960
Tomáš Tobrman <30987354+TomasTobrman@users.noreply.github.com>
2023-11-30 10:19:48 +0100

    Load Colorizer on keybind

--- lua/tirea/plugins/nvim-colorizer.lua
+++ lua/tirea/plugins/nvim-colorizer.lua
@@ -1,9 +1,12 @@
 return {
 	'norcalli/nvim-colorizer.lua',
-	event = { 'BufReadPre', 'BufNewFile' },
+	keys = { { '<leader>tc', '<cmd>ColorizerToggle<cr>', { mode = 'n', desc = 'Toggle Colorizer' } } },
 	config = function()
 		require('colorizer').setup({
-			'*',
+			['*'] = {
+				RGB = false,
+				names = false,
+			},
 			css = {
 				css = true,
 				css_fn = true,
