6c0dd3f79207f6ff4cd804d17dd8238b3e2ba33a
Tomáš Tobrman <30987354+TomasTobrman@users.noreply.github.com>
2023-12-05 20:13:36 +0100

    Bind <esc> to exit Terminal mode

--- lua/tirea/core/mappings.lua
+++ lua/tirea/core/mappings.lua
@@ -2,3 +2,5 @@ vim.keymap.set('n', '<c-h>', '<c-w>h')
 vim.keymap.set('n', '<c-j>', '<c-w>j')
 vim.keymap.set('n', '<c-k>', '<c-w>k')
 vim.keymap.set('n', '<c-l>', '<c-w>l')
+
+vim.keymap.set('t', '<esc>', '<c-\\><c-n>')
